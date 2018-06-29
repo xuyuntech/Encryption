6ad05a3d3e0088c891dbf6fe118c7403e7680e7c
windghoul <windghoul@gmail.com>
2018-06-29 12:38:50 +0800

    解决超过十五位之后加密问题

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 var crypto = require('crypto');
 var fs = require('fs');
 var path = require('path');
-var publicPem = fs.readFileSync(path.join(__dirname, "./pub.pem"));
-var privatePem = fs.readFileSync(path.join(__dirname, "./pri.pem"));
-const prikey = privatePem.toString();
-const pubKey = publicPem.toString();
+var content = '320705199402031519';
+function encrypt(content) {
+    var publicPem = fs.readFileSync(path.join(__dirname, "./pub.pem"));
+    var privatePem = fs.readFileSync(path.join(__dirname, "./pri.pem"));
+    const prikey = privatePem.toString();
+    const pubKey = publicPem.toString();
+    var iv = crypto.randomBytes(128/8);
+    var key = crypto.randomBytes(256/8);
+    var clearEncoding = 'utf8';
+    var cipherEncoding = 'base64'; /*加密*/
+    var algorithm = 'aes-256-cbc';
+    console.log('原始数据：' + content)
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    var cipherChunks = [];
+    cipherChunks.push(cipher.update(content, clearEncoding, cipherEncoding));
+    cipherChunks.push(cipher.final(cipherEncoding));
+    console.log('AES加密后数据：' + cipherChunks.join(''));
+    var crypted = crypto.publicEncrypt(pubKey, key); // 加密
+    console.log('RSA加密后密码： ' + crypted);
+    const decrypted = crypto.privateDecrypt(prikey, crypted); // 解密
+    console.log('RSA解密后密码： ' + decrypted);
+    // const encrypted =
+    // '9ca277d880d50e33fd80032ee670b8d8';
+    const decipher = crypto.createDecipheriv(algorithm, key,iv);
+    var plainChunks = [];
+    for (var i = 0; i < cipherChunks.length; i++) {
+        plainChunks.push(decipher.update(cipherChunks[i], cipherEncoding, clearEncoding));
+    
+    }
+    plainChunks.push(decipher.final(clearEncoding));
+    console.log("AES解密后数据: " + plainChunks.join(''));
 
-var content = 'hello';
-var iv = crypto.randomBytes(128/8);
-var key = crypto.randomBytes(192/8);
-var algorithm = 'aes192';
-console.log('原始数据：' + content)
-var cipher = crypto.createCipheriv(algorithm, key,iv);
-cipher.update(content);
-var cryptedContent = cipher.final('hex');
-console.log('AES加密后数据：'+ cryptedContent);
-
-var crypted = crypto.publicEncrypt(pubKey,key); // 加密
-console.log('RSA加密后密码： ' + crypted);
-
-
-
-const decrypted = crypto.privateDecrypt( prikey,crypted); // 解密
-console.log('RSA解密后密码： ' + decrypted);
-
-
-var decipher = crypto.createDecipheriv(algorithm, decrypted,iv);
-decipher.update(cryptedContent, 'hex');
-var decryptedContent = decipher.final('utf8');
-console.log('AES解密后数据：' + decryptedContent);
+}
+encrypt(content);
